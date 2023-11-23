39f4c37d22701b27d40d99b38c634e6aba1a0d29
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-11-23 20:10:16 +0500

    design main page

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import SearchBar from "./SearchBar";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <SearchBar />
+    </>
+  )
+}
+
+export default Main
