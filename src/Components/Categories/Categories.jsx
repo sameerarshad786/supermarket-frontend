b947b84b06b141ed84772208a6ba77e63d553edc
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-12-15 23:11:31 +0500

    add category 'All'

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -2,7 +2,21 @@ import React, { useState, useEffect } from "react";
 import FetchCategories from "../../Utils/Categories/FetchCategories";
 
 const Categories = ({ setSearchParams }) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([{
+      "id": 1,
+      "name": "All",
+      "sub_category": null
+  }]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleOnClick(category) {
+    if (category === 'All') {
+      setSearchParams({ category: '' })
+    } else {
+      setSearchParams({ category : category })
+    }
+    setSelectedCategory(category);
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +36,14 @@ const Categories = ({ setSearchParams }) => {
           <li key={category.id}>
             <button
               className="category"
-              onClick={() => setSearchParams({ category : category.name })}
+              onClick={(e) => handleOnClick(e.target.innerText)}
+              style={{
+                borderRadius: "0.5em",
+                boxShadow:
+                  selectedCategory === category.name
+                    ? "rgba(0, 0, 0, 0.24) 0px 3px 8px"
+                    : "none"
+              }}
             >
               {category.name}
             </button>
